724dfaa8dcbf771bead8e33fbd9adc23d720ca5a
guillermoBabangit <guillermo.baban@hotmail.com>
2021-07-02 13:48:44 -0300

    spinning

---
+++ js/index.js
@@ -0,0 +1,9 @@
+window.addEventListener("load", () => {
+  const cont1 = document.querySelector("#spinner");
+  const contenedorLoader = document.querySelector(
+    ".loadingio-spinner-eclipse-9onybfsdper"
+  );
+  cont1.style.display = "none";
+  contenedorLoader.style.visibility = "hidden";
+  contenedorLoader.style.opacity = "0";
+});
