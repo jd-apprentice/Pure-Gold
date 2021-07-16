ece3d69760d458d47d89aad1d17cd6c9d14b4d15
Jonathan Dyallo <emeraldusk@gmail.com>
2021-07-16 19:37:50 -0300

    cambiando contacto

---
+++ js/contacto.js
@@ -0,0 +1,6 @@
+// SPINNER
+const H = document.querySelector("#H");
+const cont1 = document.querySelector("#spinner");
+const grabCarrito = document.querySelector("#carrito-items");
+cont1.style.display = "none";
+H.classList.remove("overflow-hidden");
