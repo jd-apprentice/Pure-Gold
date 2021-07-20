3a0374bdc7a8992605d89bccacd7a641d1bdf6fb
Jonathan Dyallo <emeraldusk@gmail.com>
2021-07-20 19:43:01 -0300

    arreglado spinner inicio

--- js/inicio.js
+++ js/inicio.js
@@ -1,5 +1,8 @@
 // SPINNER
-const H = document.querySelector("#H");
-const cont1 = document.querySelector("#spinner");
-cont1.style.display = "none";
-H.classList.remove("overflow-hidden");
+window.onload = () => {
+    const H = document.querySelector("#H");
+    const cont1 = document.querySelector("#spinner");
+    cont1.style.display = "none";
+    H.classList.remove("overflow-hidden");
+}
+
